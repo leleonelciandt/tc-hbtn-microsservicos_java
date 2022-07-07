0910f02af23ee5cf9111a13f0a94d98526173cca
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-07 14:38:19 -0300

    Task 0 - Removing Lombok

--- 0x02/construindo_API_REST/com/song/demo/Song.java
+++ 0x02/construindo_API_REST/com/song/demo/Song.java
@@ -1,15 +1,60 @@
 package com.song.demo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Song {
     private int id;
     private String nome, artista, album, anoLancamento;
+    
+    public Song() {
+    }
+
+    public Song(int id, String nome, String artista, String album, String anoLancamento) {
+        this.id = id;
+        this.nome = nome;
+        this.artista = artista;
+        this.album = album;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
 
 
     @Override
