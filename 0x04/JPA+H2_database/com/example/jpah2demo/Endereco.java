ba15a7fb7868162f3d737ed0e476995d05ba6916
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-12 14:17:26 -0300

    Task 0 - Correcting path

---
+++ 0x04/JPA+H2_database/com/example/jpah2demo/Endereco.java
@@ -0,0 +1,111 @@
+package com.example.jpah2demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro, endereco, numero, bairro, cidade, estado;
+
+    public Endereco() {
+    }
+
+    public Endereco(Long id, String logradouro, String endereco, String numero, String bairro, String cidade, String estado) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco1 = (Endereco) o;
+        return id.equals(endereco1.id) && logradouro.equals(endereco1.logradouro) && endereco.equals(endereco1.endereco) && numero.equals(endereco1.numero) && bairro.equals(endereco1.bairro) && cidade.equals(endereco1.cidade) && estado.equals(endereco1.estado);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, logradouro, endereco, numero, bairro, cidade, estado);
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "id=" + id +
+                ", logradouro='" + logradouro + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", numero='" + numero + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
